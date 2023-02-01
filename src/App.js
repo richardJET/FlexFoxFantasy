08924d095421ebbbf21a05ce06aceb9534696d94
richardJET <richardtillo@gmail.com>
2023-02-01 12:51:51 -0500

    headline images and captions loading to page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import firebase from './firebase';
-import { getDatabase, onValue, ref} from 'firebase/database';
+import { getDatabase, onValue, query, orderByChild, ref} from 'firebase/database';
 import BlogHeadline from './components/BlogHeadline';
+
 
 const App = () => {
 
   const [blogPosts, setBlogPosts] = useState([]);
   useEffect(() => {
     const database = getDatabase(firebase);
-    const dbRef = ref(database);
-
+    const dbRef = query(ref(database, 'blogPosts'), orderByChild('date'));
+    
     onValue(dbRef, res => {
-      const data = res.val().blogPosts;
+      const data = res.val();
       const newBlogPosts = [];
       for (let key in data) {
-        newBlogPosts.push(data[key]);
+        newBlogPosts.push({...data[key], id: key});
       }
+      newBlogPosts.reverse();
       setBlogPosts(newBlogPosts);
     })
   },[]);
   
   return (
     <div className="App">
-      <h1>Flex Fox Fantasy</h1>
-      <BlogHeadline blogPosts={blogPosts}/>
+      <main>
+        <BlogHeadline blogPosts={blogPosts}/>
+      </main>
     </div>
   );
 }
--- src/components/BlogHeadline.js
+++ src/components/BlogHeadline.js
@@ -1,9 +1,40 @@
 const BlogHeadline = ({blogPosts}) => {
-    console.log(blogPosts);
+    let i = 0; 
+    let order = 0;
     return(
-        <div>
-            <h2>Blog Post</h2>
-        </div>
+        <ul>
+            {blogPosts.map(({title, mainImage, id, bannerSize, category, postSummary}) => {
+                const categoryClass = `category ${category.replace(/[9 ,']/g, "").toLowerCase()}`;
+                if (i < 24){
+                    if (bannerSize === 'full'){
+                        i = i + 4;
+                        if(i % 4 === 0){
+                            order = i;
+                        } else {
+                            order = i-5;
+                        }
+                    }else{
+                        i = i + 2;
+                        order = i;
+                    }
+
+                    return(
+                        <li key={id} className={bannerSize} style={{order: order}}>
+                            <div className='mainImageContainer'>
+                                <img src={mainImage} alt="to be added"/>
+                                <div className={categoryClass}>{category}</div>
+                            </div>
+                            <h2>{title}</h2>
+                            <p className='postSummary'>{postSummary}</p>
+                        </li>
+                    )
+                }else{
+                    return(
+                        null
+                    )
+                }
+            })}
+        </ul>
     );
 }
 
