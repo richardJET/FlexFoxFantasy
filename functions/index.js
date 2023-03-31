439af1b6f9fad908cb537181b0ccd173a6034f31
richardJET <richardtillo@gmail.com>
2023-03-30 23:27:22 -0400

    changed sender email

--- functions/index.js
+++ functions/index.js
@@ -7,18 +7,18 @@ admin.initializeApp();
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "fakeflexfantasywebsite@gmail.com",
+    user: "flexfoxfantasywebsite@gmail.com",
     pass: "vnvjeczytllbdzvj",
   },
 });
 
-exports.sendNoti=functions.database.ref("/articles").onCreate( async (snapshot)=>{
+exports.noti=functions.database.ref("/articles").onCreate(async (snapshot)=>{
   // get the data that was just added to the Realtime Database
   const val = snapshot.val();
 
   // define the email options
   const mailOptions = {
-    from: "fakeflexfantasywebsite@gmail.com",
+    from: "flexfoxfantasywebsite@gmail.com",
     to: "richardtillo@gmail.com",
     subject: "Article submitted for review",
     text: `Article submitted for review: ${JSON.stringify(val)}`,
